6dafc431fd7e9eaa5508d3f886443b28d4947ed9
Hayley Pelham <hayleypelham@catalyst.net.nz>
2022-07-02 16:18:06 +1200

    Adding followers and repo data

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,29 +5,34 @@ import Button from 'react-bootstrap/Button';
 
 const Result = ({username}) => {
     const [githubUser, setGithubUser] = useState({});
-    const [githubData, setGithubData] = useState([]);
 
-    /*const fetchData = (username) => {
-        return fetch(`https://api.github.com/users/${username}`)
-          .then((response) => response.json())
-          .then((data) => setGithubData(data));
-      }      
+    async function getGithubUser (username) {
+        const data = await fetch(`https://api.github.com/users/${username}`);
+        data.json().then((response) => setGithubUser(response));
+    }
 
-    useEffect(() => {
-        fetchData({username})
-    }, []);*/
-      
+    useEffect( () => {
+        getGithubUser(username);
+      }, [username]);
+
     return (
         <section id="resultsSection" className="mt-4 d-flex align-items-center justify-content-center">
             <Card bg="light" text="dark" id="result">
-                <Card.Header>Profile</Card.Header>
+                <Card.Header><h2>Profile</h2></Card.Header>
                 <Card.Body>
                     <Card.Title>{username}</Card.Title>
-                    <Card.Text>
-                        {username} <br />
-                        {username}
-                    </Card.Text>
-                    <Button variant="secondary">Visit profile</Button>
+                    <div class="container">
+                        <div class="row">
+                            <Card.Text class="col">
+                                <ul>
+                                    <li>Followers: {githubUser.followers}</li>
+                                    <li>Repositories: {githubUser.public_repos}</li>
+                                </ul>
+                            </Card.Text>
+                            <Card.Img class="col" src={githubUser.avatar_url} />
+                        </div>
+                    </div>
+                    <Button href={githubUser.html_url} variant="secondary">Visit profile</Button>
                 </Card.Body>
             </Card>
         </section>
