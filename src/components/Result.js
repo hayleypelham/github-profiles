cbdd4082c4191f4e1267ae1a48a8dadc34691118
Hayley Pelham <hayleypelham@catalyst.net.nz>
2022-07-03 16:28:10 +1200

    Making card disappear if no user found

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,27 +6,55 @@ import Button from 'react-bootstrap/Button';
 const Result = ({username}) => {
     const [githubUser, setGithubUser] = useState({});
     const [githubRepos, setGithubRepos] = useState([]);
-    let userNotFound = false;
+    const [userFound, setUserFound] = useState(false);
 
     async function getGithubUser (username) {
         try {
+            setUserFound(true);
             const data = await fetch(`https://api.github.com/users/${username}`);
-            if (!data.ok) {
-                userNotFound = true;
-            }
             data.json().then((response) => setGithubUser(response));
             const repoData = await fetch(`https://api.github.com/users/${username}/repos?sort=created&direction=desc`);
             repoData.json().then((response) => { 
                 try {
                     setGithubRepos(response.slice(0,4));
                 } catch (err) {
-                    userNotFound = true;
+                    setUserFound(false);
+                    alert("No user found for that username");
                 }
             });
         } catch (err) {
-            userNotFound = true;
+            setUserFound(false);
+            alert("No user found for that username");
         }
-        if (userNotFound) alert("No user found for that username");
+    }
+
+    const result = (githubUser) => {
+        return (
+            <section id="resultsSection" className="mt-4 d-flex align-items-center justify-content-center">
+                <Card bg="light" text="dark" id="result">
+                    <Card.Header><h2>Profile</h2></Card.Header>
+                    <Card.Body>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col">
+                                    <Card.Title><h3><strong>{username}</strong></h3></Card.Title>
+                                    <ul className="mt-3">
+                                        <li><b>Followers</b> {githubUser.followers}</li>
+                                        <li><b>Repositories</b> {githubUser.public_repos}</li>
+                                    </ul>
+                                    <h4>Repos</h4>
+                                    <ul>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                                <Card.Img id="avatar" className="col" src={githubUser.avatar_url} />
+                            </div>
+                        </div>
+                        <Button href={githubUser.html_url} variant="secondary">Visit profile</Button>
+                    </Card.Body>
+                </Card>
+            </section>
+        )
     }
 
     useEffect( () => {
@@ -34,30 +62,9 @@ const Result = ({username}) => {
       }, [username]);
 
     return (
-        <section id="resultsSection" className="mt-4 d-flex align-items-center justify-content-center">
-            <Card bg="light" text="dark" id="result">
-                <Card.Header><h2>Profile</h2></Card.Header>
-                <Card.Body>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col">
-                                <Card.Title><h3><strong>{username}</strong></h3></Card.Title>
-                                <ul className="mt-3">
-                                    <li><b>Followers</b> {githubUser.followers}</li>
-                                    <li><b>Repositories</b> {githubUser.public_repos}</li>
-                                </ul>
-                                <h4>Most recent repos</h4>
-                                <ul>
-                                    <li></li>
-                                </ul>
-                            </div>
-                            <Card.Img id="avatar" className="col" src={githubUser.avatar_url} />
-                        </div>
-                    </div>
-                    <Button href={githubUser.html_url} variant="secondary">Visit profile</Button>
-                </Card.Body>
-            </Card>
-        </section>
+        <>
+            { userFound === true ? result(githubUser) : null }
+        </>
     );
 }
 
